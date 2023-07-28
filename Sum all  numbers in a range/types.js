2cc04cb54684da815e354b71e89d284f1a3b3789
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-07-28 22:44:53 +0100

    added

---
+++ Sum all  numbers in a range/types.js
@@ -0,0 +1,10 @@
+function main(arr) {
+    let sum = 0
+    for (let i = 0; i <= arr.length; i++) {
+       console.log(arr[i])
+    }
+    // return sum
+}
+main([1,2,3,4,5,5])
+
+// console.log(main([1,2,3,4,9]))
---
+++ types.js
@@ -0,0 +1,9 @@
+function main(arr) {
+    let sum = 0
+    for (let i = 0; i <= arr.length; i++) {
+       console.log(i)
+    }
+    // return sum
+   
+}
+main([1,2,3,4,5,6])
